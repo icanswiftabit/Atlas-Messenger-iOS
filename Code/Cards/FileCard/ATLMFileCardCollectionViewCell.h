5898135dc3a30b49c1313ae754343b9d82103dd9
Jeremy Wyld <jeremy@layer.com>
2017-04-01 14:31:17 -0700

    Add a file card which uses UIDocumentInteractionController for quicklook and open.

---
+++ Code/Cards/FileCard/ATLMFileCardCollectionViewCell.h
@@ -0,0 +1,25 @@
+//
+//  ATLMFileCardCollectionViewCell.h
+//  Atlas Messenger
+//
+//  Copyright © 2017 Layer, Inc. All rights reserved.
+//
+
+#if 0
+#pragma mark Imports
+#endif
+
+#import <Atlas/Atlas.h>
+#import "ATLMCardPresenting.h"
+
+NS_ASSUME_NONNULL_BEGIN     // {
+
+
+#if 0
+#pragma mark -
+#endif
+@interface ATLMFileCardCollectionViewCell : ATLBaseCollectionViewCell <ATLMCardPresenting>
+
+@end
+
+NS_ASSUME_NONNULL_END       // }
